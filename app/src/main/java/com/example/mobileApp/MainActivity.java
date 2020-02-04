e1bd151009ad7d61dfa1b1ba5ec724eef2627c2a
Nadhirah <zcabnbi@ucl.ac.uk>
2020-02-04 23:58:23 +0000

    Stetho added

--- app/src/main/java/com/example/mobileApp/MainActivity.java
+++ app/src/main/java/com/example/mobileApp/MainActivity.java
@@ -6,7 +6,7 @@ import androidx.room.Room;
 
 import android.os.Bundle;
 
-import com.appspector.sdk.AppSpector;
+import com.facebook.stetho.Stetho;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Stetho.initializeWithDefaults(this);
         setContentView(R.layout.activity_main);
 
         fragmentManager = getSupportFragmentManager();
@@ -31,11 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
 
 //      To start up AppSpector Connection
-        AppSpector
-                .build(this)
-                .withDefaultMonitors()
-                .run("android_Njc0ZGExMjUtMmFhMy00ZTg2LTkxN2YtNGVhMDVmZDM3OWZh");
-
+//        AppSpector
+//                .build(this)
+//                .withDefaultMonitors()
+//                .run("android_Njc0ZGExMjUtMmFhMy00ZTg2LTkxN2YtNGVhMDVmZDM3OWZh");
 
     }
 
